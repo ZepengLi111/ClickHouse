afc782fe56f7bb2753bf83c9ea86ce2bff4414cd
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-09-07 15:09:57 +0000

    Mark `Arrow` null tuple conversion test as `long`

--- tests/queries/0_stateless/05085_native_arrow_ipc_null_tuple_conversion.sh
+++ tests/queries/0_stateless/05085_native_arrow_ipc_null_tuple_conversion.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: no-fasttest
+# Tags: long, no-fasttest
 # This test requires PyArrow to write nullable structs in files and streams.
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
